f4f2919bdadeb072aa9612eaa270584b2a24b1ba
diana vilé <diana.vile@gmail.com>
2020-11-16 11:36:54 +0100

    add handleClick()

--- src/index.js
+++ src/index.js
@@ -28,6 +28,13 @@ class Board extends React.Component {
     };
   }
 
+  //TODO: add handleClick()
+    handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
     return <Square
       value={this.state.squares[i]}
